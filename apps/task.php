24c3e53e88680c50d1085807f528483a9334487d
daysu98 <sucid2916@gmail.com>
2023-12-10 18:24:13 +0800

    second-commit

---
+++ apps/task.php
@@ -0,0 +1,8 @@
+<div class="container px-6 mx-auto grid">
+   <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+      Task
+   </h2>
+   <center>
+      <p class="dark:text-gray-200">Selamat Datang di aplikasi Todolist</p>
+   </center>
+</div>
